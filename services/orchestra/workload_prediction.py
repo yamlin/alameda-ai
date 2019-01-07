a6a9b0f8460a6f87f7374d904169018905ebab33
YingHsiu <susan.lai@prophetstor.com>
2019-01-07 16:43:08 +0800

    refactor: workload prediction entrypoint.
    Modify workload prediction entrypoint to meet datahub format.

--- services/orchestra/workload_prediction.py
+++ services/orchestra/workload_prediction.py
@@ -1,11 +1,55 @@
 '''Main entrypoint for workload prediction.'''
 
-# pylint: disable=E0611
+# pylint: disable=no-name-in-module, broad-except
 import time
+import traceback
+from multiprocessing import Process
 from framework.log.logger import Logger, LogLevel
-from framework.datastore.file_dao import FileDataStore
+from services.orchestra.recommendation.data_processor import DataProcessor
 from services.orchestra.workload_prediction.workload_predictor \
     import WorkloadPredictor
+
+
+def pod_prediction():
+    """Subprocess for pod workload prediction"""
+
+    logger = Logger(name='pod_prediction',
+                    logfile='/var/log/workload_prediction_pod.log',
+                    level=LogLevel.LV_DEBUG)
+    processor = DataProcessor(logger=logger)
+    predictor = WorkloadPredictor(log=logger, processor=processor)
+
+    while True:
+
+        pod_list = processor.get_pod_list()
+        for pod in pod_list:
+            try:
+                predictor.predict(pod, 'POD')
+            except Exception:
+                logger.error(traceback.format_exc())
+
+        time.sleep(30)
+
+
+def node_prediction():
+    """Subprocess for node workload prediction"""
+
+    logger = Logger(name='node_prediction',
+                    logfile='/var/log/workload_prediction_node.log',
+                    level=LogLevel.LV_DEBUG)
+    processor = DataProcessor(logger=logger)
+    predictor = WorkloadPredictor(log=logger, processor=processor)
+
+    while True:
+
+        node_list = processor.get_node_list()
+        for node in node_list:
+            try:
+                predictor.predict(node, 'NODE')
+            except Exception:
+                logger.error(traceback.format_exc())
+
+        time.sleep(30)
 
 
 def main():
@@ -14,35 +58,13 @@ def main():
     log = Logger()
     log.info("Start workload prediction.")
 
-    # workload predictor
-    predictor_log = Logger(name='workload_prediction',
-                           logfile='/var/log/workload_prediction.log',
-                           level=LogLevel.LV_DEBUG)
-    predictor = WorkloadPredictor(log=predictor_log)
+    p_1 = Process(target=pod_prediction)
+    p_2 = Process(target=node_prediction)
 
-    # file datastore to get pod list
-    dao = FileDataStore()
-
-    while True:
-
-        pod_list = dao.read_data()
-        for k, v in pod_list.items():
-            try:
-                pod = {
-                    "namespace": k[0],
-                    "uid": k[1],
-                    "pod_name": k[2],
-                    "type": v["type"],
-                    "policy": v["policy"]
-                }
-            except (IndexError, KeyError):
-                log.error("Not predicting POD %s:%s, "
-                          "due to wrong format of pod info.", k, v)
-                continue
-
-            predictor.predict(pod)
-
-        time.sleep(60)
+    p_1.start()
+    p_2.start()
+    p_1.join()
+    p_2.join()
 
     log.info("Workload prediction is completed.")
 
